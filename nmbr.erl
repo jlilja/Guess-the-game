558fbf67a2aca1a84d413ed683e1553e286fd927
Jonas Lilja <jonas@lilja.io>
2017-08-09 08:31:47 +0200

    Updated number of args required

--- nmbr.erl
+++ nmbr.erl
@@ -2,11 +2,11 @@
 -export([start/0]).
 
 start() ->
-	game(rand:uniform(100), guess(), 1).
+	game(rand:uniform(100), 1).
 
-game(Match, Guess, Tries) ->
+game(Match, Tries) ->
 	case guess() of
-		Match -> io:format("Congratulations! You made it in ~p tries. ", [Tries])
+		Match -> io:format("Congratulations! You made it in ~p tries. ", [Tries]);
 		Guess when Guess < Match -> io:format("~p is too low. ", [Guess]), game(Match, tries(Tries));
 		Guess when Guess > Match -> io:format("~p is too high. ", [Guess]), game(Match, tries(Tries))
 	end.
